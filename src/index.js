6f116a6b1ea3c4f81f3f19472bafe33d2ba32875
stengof <lmikon007@gmail.com>
2020-02-20 10:38:13 +0300

    feat: make task typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length === 0  ) return 0;
+
+  let min = array[0];
+
+  for (let key of array) {
+    if (key < min) min = key;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length === 0  ) return 0;
+
+  let max = array[0];
+
+  for (let key of array) {
+    if (key > max) max = key;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( !Array.isArray(array) || array.length === 0  ) return 0;
+
+  return array.reduce( (accum, value) => accum + value ) / array.length;;
 }
